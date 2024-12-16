44e280b75830b2944df70275510404508cfd83b7
CAPELLAX02 <ahmtatar@gmail.com>
2024-12-17 01:43:32 +0300

    Initialize the project

---
+++ demo-service/src/test/java/com/capellax/demo_service/DemoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capellax.demo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
