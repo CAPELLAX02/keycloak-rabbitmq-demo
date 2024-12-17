20b430ab25bdb84859d6fc90d69abd9bf9871357
CAPELLAX02 <ahmtatar@gmail.com>
2024-12-18 00:51:29 +0300

    Configure the application.yml file of the notification service. RabbitMQ and Mail logic starter

---
+++ notification-service/src/main/java/com/capellax/notification_service/RabbitMQConfig.java
@@ -0,0 +1,15 @@
+package com.capellax.notification_service;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public Queue notificationQueue() {
+        return new Queue("activation-queue", true);
+    }
+
+}
